2c4efbdeb5525318b628f87387c0c71ddfec2fff
Adriano Gil <adrianogil.san@gmail.com>
2019-06-21 17:54:11 -0400

    Show command usage

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,14 +1,23 @@
 
 function smart_build()
 {
-    echo "SMART BUILD!!!"
-    START=$(date +%s);
 
-    project_type=$(p2 $SMART_BUILD_TOOLS_DIR/detect_project.py)
 
-    $SMART_BUILD_TOOLS_DIR/$project_type/smart_build_$project_type.sh $1
+    if [ -z "$1" ]
+    then
+        echo "SMART BUILD!!!"
+        START=$(date +%s);
+        project_type=$(p2 $SMART_BUILD_TOOLS_DIR/detect_project.py)
 
-    END=$(date +%s);
-    echo $((END-START)) | awk '{print int($1/60)":"int($1%60)}'
+        $SMART_BUILD_TOOLS_DIR/$project_type/smart_build_$project_type.sh $1
+
+        END=$(date +%s);
+        echo $((END-START)) | awk '{print int($1/60)":"int($1%60)}'
+    else
+        echo "Smart Build Tool - version 0.0.1"
+        echo "Usage: go to a project directory and enter: \$ b"
+    fi
+
+
 }
 alias b="smart_build"
