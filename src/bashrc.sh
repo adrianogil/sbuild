b3f7995455d398b900e9e6a796d8bf6341991613
Adriano Gil <adrianogil.san@gmail.com>
2020-07-11 18:46:43 -0400

    Added new alias

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,5 +1,5 @@
 
-function smart_build()
+function smart-build()
 {
     echo "Smart Build Tool - version 0.0.1"
 
@@ -7,11 +7,12 @@ function smart_build()
     START=$(date +%s);
     project_type=$(p2 $SMART_BUILD_TOOLS_DIR/detect_project.py)
 
-    $SMART_BUILD_TOOLS_DIR/$project_type/smart_build_$project_type.sh $1
+    $SMART_BUILD_TOOLS_DIR/$project_type/smart_build_$project_type.sh $@
 
     END=$(date +%s);
     echo $((END-START)) | awk '{print int($1/60)":"int($1%60)}'
 
 
 }
-alias b="smart_build"
+alias b="smart-build"
+alias s-build="smart-build"
