38084848c50c4c9c440377d939dedcc4b7e101b2
Adriano Gil <adrianogil.san@gmail.com>
2024-04-13 22:29:00 -0400

    refactor: changed build function to use python3 instead of python2

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -5,14 +5,12 @@ function smart-build()
 
     echo "SMART BUILD!!!"
     START=$(date +%s);
-    project_type=$(p2 $SMART_BUILD_TOOLS_DIR/detect_project.py)
+    project_type=$(p3 $SMART_BUILD_TOOLS_DIR/detect_project.py)
 
     $SMART_BUILD_TOOLS_DIR/$project_type/smart_build_$project_type.sh $@
 
     END=$(date +%s);
     echo $((END-START)) | awk '{print int($1/60)":"int($1%60)}'
-
-
 }
 alias b="smart-build"
 alias s-build="smart-build"
